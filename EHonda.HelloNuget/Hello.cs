8fc409bbc40036fe2f1e6aa1630a03fad302d677
Dennis Renz <dennis@r-renz.de>
2023-02-26 23:40:10 +0100

    Version 0.3.0

--- EHonda.HelloNuget/Hello.cs
+++ EHonda.HelloNuget/Hello.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using System.Diagnostics;
+using JetBrains.Annotations;
 
 namespace EHonda.HelloNuget;
 
@@ -6,4 +7,19 @@ namespace EHonda.HelloNuget;
 public static class Hello
 {
     public static string Nuget => "Hello Nuget";
+
+    // Function to test stepping into / stepping through functions
+    public static int RandomSum()
+    {
+        var x = RandomInt(10);
+        var y = RandomInt(20);
+        return x + y;
+    }
+
+    [DebuggerStepThrough]
+    private static int RandomInt(int upper) => Random.Shared.Next(upper);
+    
+    // Function to test what happens if we make JetBrains.Annotations private
+    [Pure]
+    public static string Pure() => nameof(Pure);
 }
